97f80ddd9e41b1333cf4ad013d0433888d864b30
Aniket Raut <rautaniket640@gmail.com>
2024-05-02 12:37:34 +0530

    footer manupulated

--- src/Components/Footer/MyFooter.jsx
+++ src/Components/Footer/MyFooter.jsx
@@ -4,7 +4,7 @@ import Logo from './logo.png';
 import meta from './meta.svg';
 import linkedin from './linkedin.svg';
 import instagram from './instagram.svg';
-import {useState } from 'react';
+import { useState } from 'react';
 import { GiHamburgerMenu } from "react-icons/gi";
 
 
@@ -17,57 +17,52 @@ function MyFooter() {
                 <img src={Logo} alt="" />
             </div>
 
-            <ul className="column">
-                <li><a href="#">Insight</a></li>
-                <li><a href="#">Industries</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact us</a></li>
-                <li><a href="#">Investors</a></li>
-            </ul>
-
-            <ul className="column">
-                <li><a href="#">Insight</a></li>
-                <li><a href="#">Industries</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact us</a></li>
-                <li><a href="#">Investors</a></li>
-            </ul>
-
-            <ul className="column">
-                <li><a href="#">Insight</a></li>
-                <li><a href="#">Industries</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact us</a></li>
-                <li><a href="#">Investors</a></li>
-            </ul>
-
-            <ul className="column">
-                <li><a href="#">Accessibility</a></li>
-                <li><a href="#">Cookie policy</a></li>
-                <li><a href="#">Cookie settings</a></li>
-                <li><a href="#">Privacy notice</a></li>
-                <li><a href="#">Security vulnerability notification</a></li>
-                <li><a href="#">SpeakUp</a></li>
-                <li><a href="#">Terms of use</a></li>
-            </ul>
-
-              {/* hamburget menu start  */}
-          <div className="Footerhamburger-menu">
-            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
-            <GiHamburgerMenu />
-            </a>
-          </div>
-
             <div className="container">
+                <ul className="column">
+                    <li><a href="#">Insight</a></li>
+                    <li><a href="#">Industries</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Investors</a></li>
+                </ul>
+
+                <ul className="column">
+                    <li><a href="#">Insight</a></li>
+                    <li><a href="#">Industries</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Investors</a></li>
+                </ul>
+
+                <ul className="column">
+                    <li><a href="#">Insight</a></li>
+                    <li><a href="#">Industries</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Investors</a></li>
+                </ul>
+
+                <ul className="column">
+                    <li><a href="#">Accessibility</a></li>
+                    <li><a href="#">Cookie policy</a></li>
+                    <li><a href="#">Cookie settings</a></li>
+                    <li><a href="#">Privacy notice</a></li>
+                    <li><a href="#">Security vulnerability notification</a></li>
+                    <li><a href="#">SpeakUp</a></li>
+                    <li><a href="#">Terms of use</a></li>
+                </ul>
+            </div>
+
+            <div className="copywright">
                 <p>Copyright &copy; 2024 Your Company Name. All rights reserved.</p>
                 <img src={linkedin} alt="" />
                 <img src={meta} alt="" />
